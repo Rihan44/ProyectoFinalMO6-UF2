1605a96f7d2cae28712d47485fb1919d4209a4be
Rihan44 <asmuela_bustamante@hotmail.es>
2023-11-09 23:01:26 +0100

    [Feature] asteriscos añadidos

--- js/scripts.js
+++ js/scripts.js
@@ -10,7 +10,6 @@ btn_burger.addEventListener('click', () =>{
     btn_burger.style.display = 'none';
     nav_menu.style.height = '220px';
     menu_mobile.style.top = '75px';
-    console.log('ey')
 });
 
 btn_close.addEventListener('click', () => {
@@ -22,51 +21,36 @@ btn_close.addEventListener('click', () => {
 
 // ASTERISCOS
 
-function generadorPiramide() {
-    let numero_asteriscos = 0;
-    let input_asteriscos = document.querySelector('#input_asteriscos');
-
-    do {
-        numero_asteriscos = parseInt(input_asteriscos.value);
-    } while (isNaN(Number(numero_asteriscos) || numero_asteriscos <= 0))
-
-    for (let i = 1; i <= numero_asteriscos; i++) {
-        for (let j = 1; j <= i; j++) {
-            document.write("* ")
-        }
-        document.write("<br>")
-
-    }
-}
-
-let numero_asteriscos = 0; // 23 es el maximo que puedo meter
+let numero_asteriscos = 0;
 let input_asteriscos = document.querySelector('#input_asteriscos');
 let form_asteriscos = document.querySelector('#form_asteriscos');
 let contenedor_piramide = document.querySelector('#contenedor_piramide');
 let btn_reset_asteriscos = document.querySelector('#btn_reset-asteriscos'); 
-let contador_asteriscos = 0;
-// HHacer el div de un tamaño especifico y ponerle un overflow hidden
 
-form_asteriscos.addEventListener('submit', (e) => {
-    e.preventDefault();
-
-    do {
-        numero_asteriscos = parseInt(input_asteriscos.value);
-    } while (isNaN(numero_asteriscos || numero_asteriscos <= 0))
+if(form_asteriscos !== null) {
+    form_asteriscos.addEventListener('submit', (e) => {
+        e.preventDefault();
+        btn_reset_asteriscos.style.display = 'block';
+        contenedor_piramide.style.height = '380px';
     
-    for (let i = 1; i <= numero_asteriscos; i++) {
-        for (let j = 1; j <= i; j++) {
-            contenedor_piramide.innerHTML += "* ";
-            contador_asteriscos++; // 276 es el máximo
+        do {
+            numero_asteriscos = parseInt(input_asteriscos.value);
+        } while (isNaN(numero_asteriscos || numero_asteriscos <= 0))
+        
+        for (let i = 1; i <= numero_asteriscos; i++) {
+            for (let j = 1; j <= i; j++) {
+                contenedor_piramide.innerHTML += "* ";
+            }
+            contenedor_piramide.innerHTML += "<br>";
         }
-        contenedor_piramide.innerHTML += "<br>";
-    }
+    });
+}
 
-    console.log(contador_asteriscos);
-})
+if(btn_reset_asteriscos !== null){
+    btn_reset_asteriscos.addEventListener('click', ()=>{
+        location.reload();
+    });
+}
 
-btn_reset_asteriscos.addEventListener('click', ()=>{
-    location.reload();
-})
 
 
